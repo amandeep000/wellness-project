3b95e00899619baf486ec9d01c27de2c7c85a9eb
amandeep <deepaman0556@gmail.com>
2025-08-21 21:21:11 +0530

    fix:add protocol to base url for backend

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "wellness-backend-production-9a55.up.railway.app",
+  baseURL: "https://wellness-backend-production-9a55.up.railway.app",
   withCredentials: true,
 });
 
