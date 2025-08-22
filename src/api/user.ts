4005ec9eda23466f932f54adc2bab3d1c7b49b8e
amandeep <deepaman0556@gmail.com>
2025-08-22 17:19:44 +0530

    refactored user wrapper in axios

--- src/api/user.ts
+++ src/api/user.ts
@@ -2,15 +2,8 @@ import api from "./axios";
 import { User } from "../types/user";
 
 export const getCurrentUser = async (): Promise<User | null> => {
-  try {
-    const res = await api.get("/api/v1/user/me", { withCredentials: true });
-    return res.data;
-  } catch (error: any) {
-    if (error.response?.status === 401) {
-      return null;
-    }
-    throw error;
-  }
+  const res = await api.get("/api/v1/user/me");
+  return res.data;
 };
 
 export const updateProfile = async (data: any) => {
@@ -29,11 +22,11 @@ export const addAddress = async (data: any) => {
 };
 
 export const updateAddress = async (addressId: string, data: any) => {
-  const res = await api.put(`/api/v1/addresses/:${addressId},data`);
+  const res = await api.put(`/api/v1/addresses/${addressId},data`);
   return res.data;
 };
 
 export const deleteAddress = async (addressId: string) => {
-  const res = await api.delete("/api/v1/addresses/:${addressId}");
+  const res = await api.delete(`/api/v1/addresses/${addressId}`);
   return res.data;
 };
