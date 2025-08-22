3ef573c6b62ed4e320dd81888e4a9c1aa579ab5f
amandeep <deepaman0556@gmail.com>
2025-08-22 15:20:10 +0530

    minor issues fixed

--- src/routes/protectedRoutes.tsx
+++ src/routes/protectedRoutes.tsx
@@ -1,16 +1,13 @@
-import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 
 const ProtectedRoutes = () => {
   const { data: user, isLoading } = useAuth();
-  const location = useLocation();
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
-  if (!user) return <Navigate to={"/login"} replace />;
-  if (location.pathname === "/account")
-    return <Navigate to={"/account/profile"} replace />;
+
+  if (isLoading) return <p>Loading...</p>;
+  if (!user) return <Navigate to="/login" replace />;
 
   return <Outlet />;
 };
+
 export default ProtectedRoutes;
