f881d2181514de5ad89f484bd4e38d20303d0a20
amandeep <deepaman0556@gmail.com>
2025-08-22 14:57:43 +0530

    fix:returned outlet so that profile page can render after login

--- src/routes/protectedRoutes.tsx
+++ src/routes/protectedRoutes.tsx
@@ -1,13 +1,16 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 
 const ProtectedRoutes = () => {
   const { data: user, isLoading } = useAuth();
+  const location = useLocation();
   if (isLoading) {
     return <p>Loading...</p>;
   }
-  if (user) return <Navigate to={"/login"} replace />;
+  if (!user) return <Navigate to={"/login"} replace />;
   if (location.pathname === "/account")
     return <Navigate to={"/account/profile"} replace />;
+
+  return <Outlet />;
 };
 export default ProtectedRoutes;
